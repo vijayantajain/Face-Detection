706f476bcf70d36bb552b16a28ff18a098780ee7
vijayantajain <vijayantashah@gmail.com>
2018-08-23 12:56:20 +0530

    Move `allowed_extensions` to forms.py module

--- face_detector/detect/forms.py
+++ face_detector/detect/forms.py
@@ -1,7 +1,11 @@
-"""Module to define the form"""
+"""Module for defining the form as well check-functions for form-input"""
+
+from os import path
 
 from flask_wtf import FlaskForm
 from wtforms import FileField, FloatField, validators
+
+ALLOWED_EXTENSIONS = ['.jpeg', '.jpg', '.png']
 
 class InputForm(FlaskForm):
     """A WTForm used by Jinja to generate HTML
@@ -15,4 +19,32 @@ class InputForm(FlaskForm):
     confidence = FloatField(
         'Confidence', [validators.DataRequired('Confidence is required and must be a float'),
                        validators.NumberRange(min=0.0, max=0.999)])
-    image = FileField('Upload an Image')
+    image = FileField('Upload an Image', )
+
+def allowed_extension(filename):
+    """Checks the file name of the uploaded image
+
+    This function checks if the uploaded image is either jpeg or png.
+    Other image formats are not supported
+
+    Parameters
+    ----------
+    filename : str
+        Name of the file
+
+    Raises
+    ------
+    ValidationError
+        When the file name is not `ALLOWED_EXTENSIONS`
+
+    Returns
+    -------
+    val : bool
+        If the filename extension is allowed or not
+    """
+
+    val = False
+    if path.splitext(filename)[-1].lower() in ALLOWED_EXTENSIONS:
+        val = True
+
+    return val
--- face_detector/detect/views.py
+++ face_detector/detect/views.py
@@ -6,13 +6,12 @@ from os import path
 from flask import Blueprint, flash, render_template, request
 from werkzeug.utils import secure_filename
 
-from face_detector.detect.forms import InputForm
+from face_detector.detect.forms import InputForm, allowed_extension
 from face_detector.detect.detect_faces import get_faces
 
 CWD = os.getcwd()
 UPLOAD_FOLDER = path.join(CWD, 'face_detector', 'static', 'images')
 DEFAULT_IMAGE = 'obama.png'
-ALLOWED_EXTENSIONS = ['.jpeg', '.jpg', '.png']
 
 detect_faces = Blueprint(__name__, 'detect_faces')
 
@@ -36,8 +35,8 @@ def process_form():
             filename = DEFAULT_IMAGE
 
         confidence = form.confidence.data
-        image_w_faces = get_faces(filename, confidence)
-        return render_template('faces.html', filename=image_w_faces)
+        image_w_faces, num_faces = get_faces(filename, confidence)
+        return render_template('faces.html', filename=image_w_faces, num_faces=num_faces)
 
     flash_errors(form)
     return render_template('form.html', form=form)
@@ -54,28 +53,3 @@ def flash_errors(form):
     for fields, errors in form.errors.items():
         for error in errors:
             flash('Error in the field {} - {}'.format(getattr(form, fields).label.text, error))
-
-def allowed_extension(filename):
-    """Checks the file name of the uploaded image
-
-    Parameters
-    ----------
-    filename : str
-        Name of the file
-
-    Raises
-    ------
-    ValidationError
-        When the file name is not `ALLOWED_EXTENSIONS`
-
-    Returns
-    -------
-    val : bool
-        If the filename extension is allowed or not
-    """
-
-    val = False
-    if path.splitext(filename)[-1] in ALLOWED_EXTENSIONS:
-        val = True
-
-    return val
