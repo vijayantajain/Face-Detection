e625e3a4573e94d454f207da0241387dc3ee6e41
vijayantajain <vijayantashah@gmail.com>
2018-08-24 15:20:38 -0500

    Add custom 404 handler

--- face_detector/__init__.py
+++ face_detector/__init__.py
@@ -3,7 +3,7 @@
 import os
 from os import path
 
-from flask import Flask
+from flask import Flask, render_template
 from face_detector.detect.views import detect_faces
 
 CWD = os.getcwd()
@@ -12,3 +12,7 @@ INSTANCE_PATH = path.join(CWD, 'instance')
 app = Flask(__name__, instance_path=INSTANCE_PATH, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 app.register_blueprint(detect_faces)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
