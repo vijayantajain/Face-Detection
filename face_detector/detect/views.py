1eb04e92350ff383964450b87bb3250235d37cbb
vijayantajain <vijayantashah@gmail.com>
2018-08-26 13:18:48 -0500

    Switch to class based views

--- face_detector/detect/views.py
+++ face_detector/detect/views.py
@@ -4,6 +4,7 @@ import os
 from os import path
 
 from flask import Blueprint, flash, render_template, request
+from flask.views import MethodView
 from werkzeug.utils import secure_filename
 
 from face_detector.detect.forms import InputForm, allowed_extension
@@ -15,41 +16,79 @@ DEFAULT_IMAGE = 'obama.png'
 
 detect_faces = Blueprint(__name__, 'detect_faces')
 
-@detect_faces.route('/', methods=['GET', 'POST'])
-def process_form():
-    """This function extracts the image and the confidence
-    from the http-request and returns the image with
-    bounding boxes around faces.
+class FormView(MethodView):
+    """Class handling the view of the form.
+
+    There are two methods `get` and `set` which
+    handle the respective request and accordingly
+    returns the `render_template` function which
+    renders the required template
     """
-    form = InputForm(request.form)
-    if request.method == 'GET':
+
+    def get(self):
+        """This method handles the GET request from the user
+        which is when the main page is loaded.
+
+        This method returns the form.html page with context as
+        Input form
+
+        Returns
+        -------
+        render_template : function
+            The function which renders the form.html
+        """
+        form = InputForm(request.form)
         return render_template('form.html', form=form)
 
-    if request.method == 'POST' and form.validate():
-        try:
-            image = request.files['image']
-            if image and allowed_extension(image.filename):
-                filename = secure_filename(image.filename)
-                image.save(path.join(UPLOAD_FOLDER, filename))
-        except KeyError:
-            filename = DEFAULT_IMAGE
+    def post(self):
+        """Handles the POST request from the user
 
-        confidence = form.confidence.data
-        image_w_faces, num_faces = get_faces(filename, confidence)
-        return render_template('faces.html', filename=image_w_faces, num_faces=num_faces)
+        This method handles the request after the form
+        has been filled. First it checks for the validity
+        of the inputs through `form.validate` function. If
+        the input data in the form is correct then it saves
+        the image and calls `get_faces` to get the image file
+        containing the detected faces and the number of faces
+        detected. It then returns the `render_template` function
+        which renders `faces.html` template with the requisite 
+        context. In case the form input is not valid it 'flashes'
+        the respective error and renders the 'form.html' template
 
-    flash_errors(form)
-    return render_template('form.html', form=form)
+        Returns
+        -------
+        render_template : function
+            The function which renders template
+        """
 
-def flash_errors(form):
-    """Function for flashing errors for an invalid input in the form
+        form = InputForm(request.form)
+        if form.validate():
+            try:
+                image = request.files['image']
+                if image and allowed_extension(image.filename):
+                    filename = secure_filename(image.filename)
+                    image.save(path.join(UPLOAD_FOLDER, filename))
+            except KeyError:
+                filename = DEFAULT_IMAGE
 
-    Parameters
-    ----------
-    form : ProfileForm
-        Form object containing fields and errors
+            confidence = form.confidence.data
+            image_w_faces, num_faces = get_faces(filename, confidence)
+            return render_template('faces.html', filename=image_w_faces, num_faces=num_faces)
 
-    """
-    for fields, errors in form.errors.items():
-        for error in errors:
-            flash('Error in the field {} - {}'.format(getattr(form, fields).label.text, error))
+        self.flash_errors(form)
+        return render_template('form.html', form=form)
+
+    @staticmethod
+    def flash_errors(form):
+        """Function for flashing errors for an invalid input in the form
+
+        Parameters
+        ----------
+        form : ProfileForm
+            Form object containing fields and errors
+
+        """
+        for fields, errors in form.errors.items():
+            for error in errors:
+                flash('Error in the field {} - {}'.format(getattr(form, fields).label.text, error))
+
+detect_faces.add_url_rule(rule='/', view_func=FormView.as_view('home'))
